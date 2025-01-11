510a502ec6aa21b4d3db4572a55f1931ed825f86
Yash Patel <yash.patel0419@gmail.com>
2025-01-11 13:31:32 -0500

    updated codebase to use picamera2 for compatibility with rpi camera module, updated codebase to use GTK instead of cv2.imshow, added dependencies to dockerfile for picamera2 and GTK, added bind mounts to docker for src/ directory for easier testing of code iterations inside container

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export DISPLAY=:0
+xhost +local:root
+xhost +local:docker
 
 # Define image and container names
 IMAGE_NAME="picar:latest"
@@ -9,18 +11,25 @@ CONTAINER_NAME="picar-container"
 # Build the image
 echo "Building the image..."
 sudo docker build -t $IMAGE_NAME -f docker/Dockerfile .
+
 # Remove existing container if it exists
 echo "Removing existing container (if any)..."
 sudo docker rm -f $CONTAINER_NAME 2>/dev/null || true
 
-# Run the container
+# Run the container with a bind mount for the /src folder
 echo "Running the container..."
 sudo docker run --name $CONTAINER_NAME -it \
     --device /dev/bus/usb \
     --device /dev/video0:/dev/video0 \
+    --device /dev/vchiq:/dev/vchiq \
     --device /dev/dri:/dev/dri \
     -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v /run/udev:/run/udev:ro \
+    -v "$(pwd)/src:/app/src" \
+    --privileged \
+    --ipc=host \
     $IMAGE_NAME
 
 # Clean up dangling images and stopped containers
